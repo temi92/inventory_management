1da58f6d5f51bf8d21ab98432d9c5c9a7037ae7e
temi <odusinatemitope14@yahoo.com>
2020-02-28 17:46:26 +0000

    database changed

--- deets_nigeria/__init__.py
+++ deets_nigeria/__init__.py
@@ -11,7 +11,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+os.path.join(basedir, "tmp.sqlite")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres+psycopg2://postgres:folajoke92@localhost:5432/postgres1"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres+psycopg2://postgres:folajoke92@localhost:5432/postgres1"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://bsahjytqgesxgp:f1e7df4254aa4c92972e98e1f1eba467edb151a0b00c95672fe38736e843473d@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d6714i5vq189v1"
+
 
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://qvaacilwhlandq:29fa1dea878d34b8cd47bc251b0822834da0addb464b55b509d5dbe18dd53462@ec2-174-129-33-147.compute-1.amazonaws.com:5432/dfc99jep0jm4ta"
 app.config[" SQLALCHEMY_TRACK_MODIFICATIONS "] = False
