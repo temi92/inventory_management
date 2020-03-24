479adfe1781324ff763c402fab3ca9f35ca2bf63
temi <odusinatemitope14@yahoo.com>
2020-03-24 20:24:31 +0000

    updated db address

--- deets_nigeria/__init__.py
+++ deets_nigeria/__init__.py
@@ -10,8 +10,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres+psycopg2://postgres:folajoke92@localhost:5432/postgres1"
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://bsahjytqgesxgp:f1e7df4254aa4c92972e98e1f1eba467edb151a0b00c95672fe38736e843473d@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d6714i5vq189v1"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres+psycopg2://postgres:folajoke92@localhost:5432/postgres1"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://bsahjytqgesxgp:f1e7df4254aa4c92972e98e1f1eba467edb151a0b00c95672fe38736e843473d@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d6714i5vq189v1"
 
 
 app.config[" SQLALCHEMY_TRACK_MODIFICATIONS "] = False
